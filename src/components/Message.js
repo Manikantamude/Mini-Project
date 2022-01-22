9ea6bd1d7940dc9328e411c4be1337d5eab3071d
Manikanta mude <manikantamude18@gmail.com>
2022-01-22 11:41:46 +0530

    add the date method

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 class Message extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            time:new Date().toLocaleString('en-us',{hour:'numeric',minute:'numeric',hour12:true,second:'numeric'})
+        }
+    }
     render(){
         return(
             <div>
-                <p>{this.props.msg}</p>
+                <p>{this.props.msg}<span>{this.state.time}</span> </p>
             </div>
         )
     }
